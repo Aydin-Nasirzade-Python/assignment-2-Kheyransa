c59e0e7238853ab757c88685ee532bac29537aa4
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2024-04-07 14:44:19 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -6,8 +6,9 @@ def main():
       print("Entered alphabet is a consonant!")
   elif a=="y":
       print("Sometimes it is a vowel, and sometimes it is a consonant!")
-  else:
+  elif a=="b" or a=="c" or a=="d" or a=="f" or a=="g" or a=="h" or a=="j" or a=="k" or a=="l" or a=="m" or a=="n" or a=="p" or a=="q" or a=="r" or a=="s" or a=="t" or a=="v" or a=="w" or a=="x" or a=="y" or a=="z":
       print("Entered alphabet is a vowel!")
+    
   pass
 
 if __name__ == "__main__":
